7ba8b1a3285b792ca60792b796ba2392369a2acc
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2022-03-23 14:10:18 -0500

    Update Jenkinsfile

--- update.js
+++ update.js
@@ -3,26 +3,26 @@ const execShPromise = require("exec-sh").promise;
 let fs = require("fs");
 
 const projects = [
-  { name: "202212_Equipo01" },
-  { name: "202212_Equipo02" },
+  //{ name: "202212_Equipo01" },
+  //{ name: "202212_Equipo02" },
   //{ name: "202212_Equipo03" },
   //{ name: "202212_Equipo04" },
   //{ name: "202212_Equipo05" },
-  //{ name: "202212_Equipo06" },
-  //{ name: "202212_Equipo07" },
-  //{ name: "202212_Equipo08" },
-  //{ name: "202212_Equipo09" },
-  //{ name: "202212_Equipo10" },
-  //{ name: "202212_Equipo11" },
-  //{ name: "202212_Equipo12" },
-  // { name: "202212_Equipo13" },
-  // { name: "202212_Equipo14" },
-  // { name: "202212_Equipo15" },
-  // { name: "202212_Equipo16" },
-  // { name: "202212_Equipo17" },
-  // { name: "202212_Equipo18" },
-  // { name: "202212_Equipo19" },
-  // { name: "202212_Equipo20" },
+  // { name: "202212_Equipo06" },
+  // { name: "202212_Equipo07" },
+  // { name: "202212_Equipo08" },
+  // { name: "202212_Equipo09" },
+  // { name: "202212_Equipo10" },
+  { name: "202212_Equipo11" },
+  { name: "202212_Equipo12" },
+  { name: "202212_Equipo13" },
+  { name: "202212_Equipo14" },
+  { name: "202212_Equipo15" },
+  { name: "202212_Equipo16" },
+  { name: "202212_Equipo17" },
+  { name: "202212_Equipo18" },
+  { name: "202212_Equipo19" },
+  { name: "202212_Equipo20" },
   // { name: "202212_Equipo21" },
   // { name: "202212_Equipo22" },
   // { name: "202212_Equipo23" },
@@ -49,31 +49,19 @@ const updateRepos = async () => {
   let out;
   try {
     for (const project of projects) {
-      let command1 = `git remote rm origin && git add . &&
-       git commit -m "Update Jenkinsfile and sonar-project.properties" &&
-       git remote add origin git@github.com:MISW-4104-Web/${project.name}.git &&
-       git pull origin master &&
-       git push origin master`;
-      out = await execShPromise(command1, true);
-
       const jenkinsFile = getJenkinsFile(project.name);
       const sonarFile = getSonarFile(project.name);
 
       fs.writeFileSync("Jenkinsfile", jenkinsFile);
       fs.writeFileSync("sonar-project.properties", sonarFile);
 
-      let command2 = `git remote rm origin &&
-       git add . &&
-       git commit -m "Update Jenkinsfile and sonar-project.properties" &&
-       git remote add origin git@github.com:MISW-4104-Web/${project.name}.git &&
-       git push origin master`;
-      out = await execShPromise(command2, true);
-
-      fs.unlinkSync("Jenkinsfile");
-      fs.unlinkSync("sonar-project.properties");
-
-      let command3 = `git reset --hard`;
-      out = await execShPromise(command3, true);
+      let command1 = `git remote rm origin &&
+        git remote add origin git@github.com:MISW-4104-Web/${project.name}.git &&
+        git add . &&
+        git commit -m "Update Jenkinsfile" &&
+        git fetch origin master &&
+        git merge -s recursive -X theirs origin/master`;
+      out = await execShPromise(command1, true);
     }
   } catch (e) {
     console.log("Error: ", e);
@@ -99,7 +87,6 @@ function getSonarFile(repo) {
   sonar.ts.tslint.configPath=tslint.json
   sonar.javascript.lcov.reportPaths=coverage/front/lcov.info
   sonar.testExecutionReportPaths=reports/ut_report.xml`;
-
   return content;
 }
 
