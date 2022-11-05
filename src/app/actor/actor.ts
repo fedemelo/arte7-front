c3a415c433fe1d18e215e1e9be46de7d4946ce4e
Federico Melo Barrero <f.melo@uniandes.edu.co>
2022-11-05 17:28:45 -0500

    Getters y setters

--- src/app/actor/actor.ts
+++ src/app/actor/actor.ts
@@ -1,19 +1,100 @@
 export class Actor{
-nombre: string;
-fotografia: string;
-nacionalidad: string;
-fechaNacimiento: any;
-biografia: string;
+  private _nombre: string;
+  private _fotografia: string;
+  private _nacionalidad: string;
+  private _fechaNacimiento: any;
+  private _biografia: string;
 
-constructor
-(nombre: string,
-fotografia: string,
-nacionalidad: string,
-fechaNacimiento: any,
-biografia: string)
-{this.nombre=nombre;
-this.fotografia=fotografia;
-this.nacionalidad=nacionalidad;
-this.fechaNacimiento=fechaNacimiento;
-this.biografia=biografia;}
+  constructor
+  (_nombre: string,
+  _fotografia: string,
+  _nacionalidad: string,
+  _fechaNacimiento: any,
+  _biografia: string)
+  {this._nombre = _nombre;
+  this._fotografia = _fotografia;
+  this._nacionalidad = _nacionalidad;
+  this._fechaNacimiento = _fechaNacimiento;
+  this._biografia = _biografia;}
+
+    /**
+     * Getter nombre
+     * @return {string}
+     */
+	public get nombre(): string {
+		return this._nombre;
+	}
+
+    /**
+     * Getter fotografia
+     * @return {string}
+     */
+	public get fotografia(): string {
+		return this._fotografia;
+	}
+
+    /**
+     * Getter nacionalidad
+     * @return {string}
+     */
+	public get nacionalidad(): string {
+		return this._nacionalidad;
+	}
+
+    /**
+     * Getter fechaNacimiento
+     * @return {any}
+     */
+	public get fechaNacimiento(): any {
+		return this._fechaNacimiento;
+	}
+
+    /**
+     * Getter biografia
+     * @return {string}
+     */
+	public get biografia(): string {
+		return this._biografia;
+	}
+
+    /**
+     * Setter nombre
+     * @param {string} value
+     */
+	public set nombre(value: string) {
+		this._nombre = value;
+	}
+
+    /**
+     * Setter fotografia
+     * @param {string} value
+     */
+	public set fotografia(value: string) {
+		this._fotografia = value;
+	}
+
+    /**
+     * Setter nacionalidad
+     * @param {string} value
+     */
+	public set nacionalidad(value: string) {
+		this._nacionalidad = value;
+	}
+
+    /**
+     * Setter fechaNacimiento
+     * @param {any} value
+     */
+	public set fechaNacimiento(value: any) {
+		this._fechaNacimiento = value;
+	}
+
+    /**
+     * Setter biografia
+     * @param {string} value
+     */
+	public set biografia(value: string) {
+		this._biografia = value;
+	}
+
 }
--- src/app/pelicula/pelicula.ts
+++ src/app/pelicula/pelicula.ts
@@ -1,34 +1,162 @@
-
-
 export class Pelicula{
-   nombre:string;
-   poster:string;
-     duracionSec:number;
-     pais:string;
-     fechaEstreno:any;
-     url:string;
-     visitas:number;
-     estrellasPromedio:number;
+  private _nombre: string;
+  private _poster: string;
+  private _duracionSec: number;
+  private _pais: string;
+  private _fechaEstreno: any;
+  private _url: string;
+  private _visitas: number;
+  private _estrellasPromedio: number;
 
 
-constructor(
-  nombre:string,
-  poster:string,
-    duracionSec:number,
-    pais:string,
-    fechaEstreno:any,
-    url:string,
-    visitas:number,
-    estrellasPromedio:number
-)
-{
-  this.nombre=nombre;
-  this.poster=poster;
-  this.duracionSec=duracionSec;
-  this.pais=pais;
-  this.fechaEstreno=fechaEstreno;
-  this.url=url;
-  this.visitas=visitas;
-  this.estrellasPromedio=estrellasPromedio;
-}
+  constructor(
+    _nombre: string,
+    _poster: string,
+    _duracionSec: number,
+    _pais: string,
+    _fechaEstreno: any,
+    _url: string,
+    _visitas: number,
+    _estrellasPromedio: number
+  )
+  {
+    this._nombre = _nombre;
+    this._poster = _poster;
+    this._duracionSec = _duracionSec;
+    this._pais = _pais;
+    this._fechaEstreno = _fechaEstreno;
+    this._url = _url;
+    this._visitas = _visitas;
+    this._estrellasPromedio = _estrellasPromedio;
+  }
+
+    /**
+     * Getter nombre
+     * @return {string}
+     */
+	public get nombre(): string {
+		return this._nombre;
+	}
+
+    /**
+     * Getter poster
+     * @return {string}
+     */
+	public get poster(): string {
+		return this._poster;
+	}
+
+    /**
+     * Getter duracionSec
+     * @return {number}
+     */
+	public get duracionSec(): number {
+		return this._duracionSec;
+	}
+
+    /**
+     * Getter pais
+     * @return {string}
+     */
+	public get pais(): string {
+		return this._pais;
+	}
+
+    /**
+     * Getter fechaEstreno
+     * @return {any}
+     */
+	public get fechaEstreno(): any {
+		return this._fechaEstreno;
+	}
+
+    /**
+     * Getter url
+     * @return {string}
+     */
+	public get url(): string {
+		return this._url;
+	}
+
+    /**
+     * Getter visitas
+     * @return {number}
+     */
+	public get visitas(): number {
+		return this._visitas;
+	}
+
+    /**
+     * Getter estrellasPromedio
+     * @return {number}
+     */
+	public get estrellasPromedio(): number {
+		return this._estrellasPromedio;
+	}
+
+    /**
+     * Setter nombre
+     * @param {string} value
+     */
+	public set nombre(value: string) {
+		this._nombre = value;
+	}
+
+    /**
+     * Setter poster
+     * @param {string} value
+     */
+	public set poster(value: string) {
+		this._poster = value;
+	}
+
+    /**
+     * Setter duracionSec
+     * @param {number} value
+     */
+	public set duracionSec(value: number) {
+		this._duracionSec = value;
+	}
+
+    /**
+     * Setter pais
+     * @param {string} value
+     */
+	public set pais(value: string) {
+		this._pais = value;
+	}
+
+    /**
+     * Setter fechaEstreno
+     * @param {any} value
+     */
+	public set fechaEstreno(value: any) {
+		this._fechaEstreno = value;
+	}
+
+    /**
+     * Setter url
+     * @param {string} value
+     */
+	public set url(value: string) {
+		this._url = value;
+	}
+
+    /**
+     * Setter visitas
+     * @param {number} value
+     */
+	public set visitas(value: number) {
+		this._visitas = value;
+	}
+
+    /**
+     * Setter estrellasPromedio
+     * @param {number} value
+     */
+	public set estrellasPromedio(value: number) {
+		this._estrellasPromedio = value;
+	}
+
+
 }
