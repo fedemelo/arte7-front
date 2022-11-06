cea746984da4010a0e643ad2b61ae6ffaa9e70f4
Federico Melo Barrero <f.melo@uniandes.edu.co>
2022-11-06 13:06:41 -0500

    Exports bien

--- src/app/pelicula/pelicula.module.ts
+++ src/app/pelicula/pelicula.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VistaPrincipalComponent } from './vista-principal/vista-principal.component';
+import { MenuFiltrosComponent } from './menu-filtros/menu-filtros.component';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [VistaPrincipalComponent]
+  exports: [
+    VistaPrincipalComponent,
+    MenuFiltrosComponent
+  ],
+  declarations: [
+    VistaPrincipalComponent,
+    MenuFiltrosComponent
+  ]
 })
 export class PeliculaModule { }
