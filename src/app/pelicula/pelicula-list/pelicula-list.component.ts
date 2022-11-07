5d79d2568aaeabac959b740eb45fca2d40acbfcd
Juan Diego Ortega <j.ortegam@uniandes.edu.co>
2022-11-06 23:19:58 -0500

    Hay errores por todo lado

---
+++ src/app/pelicula/pelicula-list/pelicula-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pelicula-list',
+  templateUrl: './pelicula-list.component.html',
+  styleUrls: ['./pelicula-list.component.css']
+})
+export class PeliculaListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pelicula/pelicula-list/pelicula-list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PeliculaListComponent } from './pelicula-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PeliculaListComponent]
+})
+export class PeliculaListModule { }
