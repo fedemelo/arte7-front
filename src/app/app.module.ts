5e0f695d6f7278dd958a54fdd89c96e7ec515556
Worfran <fraworman@hotmail.com>
2022-11-28 13:24:06 -0500

    Update 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { DirectorRoutingModule } from './director/director-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 
--- src/app/pelicula/pelicula.module.ts
+++ src/app/pelicula/pelicula.module.ts
@@ -11,7 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { PeliculaDetailComponent } from './pelicula-detail/pelicula-detail.component';
